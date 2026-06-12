70847f915fe2f2039e4ab70d9e9ecbdc545166ce
3070646019@qq.com <3070646019@qq.com>
2026-06-12 13:12:05 +0800

    auto update ~0,4dt:~4,2dt:~6,2dt:~8,2dt:~10,2

--- 提交代码.bat
+++ 提交代码.bat
@@ -4,10 +4,10 @@ setlocal
 
 echo ==============================
 echo   WDHJ Auto Commit
-echo ==============================
+==============================
 echo.
 
-cd /d D:\CXKF\WDHJ
+cd /d E:\CHENGXU\WDHJ
 
 echo [1/4] Checking git status...
 git status --short
@@ -17,7 +17,7 @@ echo [2/4] Adding all changes...
 git add -A
 
 echo [3/4] Generating commit message...
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set dt=%%I
+for /f "tokens=2 delims==" %%I in ('powershell -command "Get-Date -Format yyyyMMddHHmmss"') do set dt=%%I
 set commitMsg=auto update %dt:~0,4%-%dt:~4,2%-%dt:~6,2% %dt:~8,2%:%dt:~10,2%
 
 echo Commit: %commitMsg%
@@ -30,6 +30,6 @@ git push origin main
 echo.
 echo ==============================
 echo   Done! Pushed to main
-echo ==============================
+==============================
 echo.
 pause
